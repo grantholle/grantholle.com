4ffc7fb697f83eb290fd4c5cb7093e6167747b59
Grant Holle <hollegrant@gmail.com>
2021-08-20 23:17:30 +0800

    Add the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
@@ -6,15 +8,16 @@ module.exports = {
       center: true,
       padding: '1.5rem',
     },
-    debugScreens: {
-      position: ['bottom', 'right'],
+    extend: {
+      colors: {
+        ...colors,
+      }
     },
-    extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('tailwindcss-debug-screens'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+    // require('tailwindcss-debug-screens'),
   ],
   purge: ['./src/**/*.{js,md,njk,svg}'],
 }
