7850e201bc23e0b5fa244814ec7df5e5359d8b88
Grant Holle <hollegrant@gmail.com>
2024-03-30 11:23:10 -0500

    Add some razmataz

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: theme => ({
+        giddyup: {
+          '0%, 100%': {
+            'background-size':'400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size':'200% 200%',
+            'background-position': 'right center'
+          }
+        },
+      }),
+      animation: {
+        giddyup: 'giddyup 10s ease infinite',
+      },
+    },
+  },
+  plugins: [],
 }
